ee4e87d161fe1dcb8dbf1bec1e0bca4c9691d280
Piotr Komar <komarenko17@gmail.com>
2018-12-10 21:27:44 +0100

    Added scalar multiplication

--- main.cpp
+++ main.cpp
@@ -7,12 +7,15 @@ int main()
 {
 
     Vector3D *v1 = new Vector3D(2,2,2);
-    Vector3D *v2 = new Vector3D(1,1,1);
+    Vector3D *v2 = new Vector3D(1,2,3);
     //wyswietlenie wartosci wektorow
     v1->displayResult(*v1);
     v2->displayResult(*v2);
     cout<<endl;
     v1->addVector(*v1,*v2);
+    v1->displayResult(*v1);
+    cout<<endl;
+    v1->scalarMultiplication(*v1,*v2);
     v1->displayResult(*v1);
     return 0;
 }
